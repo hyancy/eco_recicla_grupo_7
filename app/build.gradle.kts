97825ed954d23085a779b673f7423cc6d5407bc9
hyancy <henryyancycaballero@gmail.com>
2023-11-06 21:00:24 -0500

    Se agrega logica para login, se conecta proyecto con firebase, se agregan dependencias para auth firebase en gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,10 +35,15 @@ android {
 
 dependencies {
 
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/Login.java
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/Login.java
@@ -1,23 +1,50 @@
 package com.hyancy.eco_recicla_reto_1_grupo_7;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
 public class Login extends AppCompatActivity {
     Button iniciarSesion;
+    TextInputEditText userLogin, passwordLogin;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
     TextView tvNoTieneCuentaRegistrate;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intentPrincipal = new Intent(getApplicationContext(), Principal.class);
+            startActivity(intentPrincipal);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         initComponents();
         listenersButtons();
@@ -25,6 +52,10 @@ public class Login extends AppCompatActivity {
 
     private void initComponents() {
         iniciarSesion = findViewById(R.id.btn_iniciar_sesion);
+        userLogin = findViewById(R.id.user_login);
+        passwordLogin = findViewById(R.id.password_login);
+        progressBar = findViewById(R.id.progress_bar);
+
         tvNoTieneCuentaRegistrate = findViewById(R.id.tv_no_tiene_cuenta);
     }
 
@@ -32,13 +63,45 @@ public class Login extends AppCompatActivity {
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(initIntents().get(0));
+                progressBar.setVisibility(v.VISIBLE);
+                String user, password;
+                user = userLogin.getText().toString();
+                password = passwordLogin.getText().toString();
+
+                if (TextUtils.isEmpty(user)) {
+                    Toast.makeText(getApplicationContext(), "Ingrese su usuario", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Ingrese su contraseña", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(user, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(v.GONE);
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(getApplicationContext(), "Login exitoso!",
+                                            Toast.LENGTH_SHORT).show();
+                                    startActivity(initIntents().get(0));
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(getApplicationContext(), "Login fallido, compruebe sus datos de ingreso!.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
         tvNoTieneCuentaRegistrate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(initIntents().get(1));
+                finish();
             }
         });
     }
