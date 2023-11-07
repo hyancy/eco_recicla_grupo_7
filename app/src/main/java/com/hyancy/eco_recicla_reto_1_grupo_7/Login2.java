6711a0e18cd22753060d73e62c0de0fafa1ec18e
hyancy <henryyancycaballero@gmail.com>
2023-11-06 19:25:55 -0500

    Se corrigen efecto de imageview en background de pantallas de categorías, registro de usuario y registro de residuos

---
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/Login2.java
@@ -0,0 +1,14 @@
+package com.hyancy.eco_recicla_reto_1_grupo_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
---
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/Register2.java
@@ -0,0 +1,14 @@
+package com.hyancy.eco_recicla_reto_1_grupo_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
+}
