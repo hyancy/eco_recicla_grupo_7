7dd5f25cb51281247a50b3985d00ac25ea33fef6
Miguel <mikeluxex@gmail.com>
2023-11-29 21:46:51 -0600

    Mi primer commit

--- app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/RegistroUsario.java
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/RegistroUsario.java
@@ -1,12 +1,15 @@
 package com.hyancy.eco_recicla_reto_1_grupo_7;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,68 +24,64 @@ public class RegistroUsario extends AppCompatActivity {
     Button btnRegistrarUsuario;
     TextView tvYaTieneCuenta;
 
+    EditText etNombre;
+    EditText etEmail;
+    EditText etUsuario;
+    EditText etContrasena;
+    EditText etConfirmarContrasena;
+
+    Button btnAceptarTodo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usario);
-
-        initComponents();
-        listenersButtons();
-
-    }
-
-
-    private void initComponents() {
-        btnCerrar = findViewById(R.id.btn_cerrar);
-        cbxTerminosCondiciones = findViewById(R.id.checkbox_terminos_condiciones);
-        cbxPoliticasPrivacidad = findViewById(R.id.checkbox_privacidad);
         btnRegistrarUsuario = findViewById(R.id.btn_registar_usuario);
-        tvYaTieneCuenta = findViewById(R.id.tv_ya_tiene_cuenta);
-    }
-
-    private void listenersButtons() {
-        btnCerrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(initIntents().get(0));
-            }
-        });
-        cbxTerminosCondiciones.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(initIntents().get(1));
-            }
-        });
-        cbxPoliticasPrivacidad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(initIntents().get(1));
-            }
-        });
         btnRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(initIntents().get(2));
+                etNombre = findViewById(R.id.et_nombre);
+                etEmail = findViewById(R.id.et_email);
+                etUsuario = findViewById(R.id.et_usuario);
+                etContrasena = findViewById(R.id.et_contraseña);
+                etConfirmarContrasena = findViewById(R.id.et_confirmar_contraseña);
+                String etNombreStr = etNombre.getText().toString();
+                String etEmailStr = etEmail.getText().toString();
+                String etUsuarioStr = etUsuario.getText().toString();
+                String etContrasenaStr = etContrasena.getText().toString();
+                String etConfirmarStr = etConfirmarContrasena.getText().toString();
+                if(etNombreStr.isEmpty() || etEmailStr.isEmpty() || etUsuarioStr.isEmpty() || etContrasenaStr.isEmpty() || etConfirmarStr.isEmpty() ) {
+                    // Primer paso para crear un dialogo: asignando valores
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RegistroUsario.this);
+                    View selector = getLayoutInflater().inflate(R.layout.dialog_completa_todo, null);
+
+                    // Segundo paso: pasando al builder
+                    builder.setView(selector);
+
+                    // Creando dialogo
+                    AlertDialog dialog = builder.create();
+                    btnAceptarTodo = findViewById(R.id.btn_acepta_todo);
+                    btnAceptarTodo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.hide();
+                        }
+                    });
+                    dialog.show();
+
+
+                } else {
+                    Intent intent = new Intent(RegistroUsario.this, Login.class);
+                    startActivity(intent);
+                }
             }
         });
-        tvYaTieneCuenta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(initIntents().get(2));
-            }
-        });
+
+
+
     }
 
-    private ArrayList<Intent> initIntents() {
-        ArrayList<Intent> listaIntents = new ArrayList<>();
-        Intent intentHome = new Intent(RegistroUsario.this, MainActivity.class);
-        Intent intentTerminosCondiciones = new Intent(RegistroUsario.this, PoliticaPrivacidadTerminos.class);
-        Intent intentLogin = new Intent(RegistroUsario.this, Login.class);
 
-        listaIntents.add(intentHome);
-        listaIntents.add(intentTerminosCondiciones);
-        listaIntents.add(intentLogin);
 
-        return listaIntents;
-    }
 }
