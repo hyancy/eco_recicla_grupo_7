3f7a51facf6b7459b6f6fed5ef14a60388e6ce6b
hyancy <henryyancycaballero@gmail.com>
2023-12-05 01:11:52 -0500

    habilitando dialogos y codigo comentado en rama miguel

--- app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/RegistroUsario.java
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/RegistroUsario.java
@@ -16,16 +16,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import com.google.android.gms.tasks.OnCompleteListener;
-//import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-//import com.google.firebase.auth.AuthResult;
-//import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
-
-import kotlin.text.RegexOption;
 
 public class RegistroUsario extends AppCompatActivity {
     FloatingActionButton btnCerrar;
@@ -35,9 +33,9 @@ public class RegistroUsario extends AppCompatActivity {
     TextView tvYaTieneCuenta;
     EditText edtName, edtEmail, edtConfirmEmail, edtPassword, edtConfirmPassword;
 
-    //FirebaseAuth mAuth;
+    FirebaseAuth mAuth;
 
-   /* @Override
+    @Override
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -47,12 +45,12 @@ public class RegistroUsario extends AppCompatActivity {
             finish();
         }
     }
-*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usario);
-        //mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         initComponents();
         listenersButtons();
@@ -80,7 +78,7 @@ public class RegistroUsario extends AppCompatActivity {
         btnCerrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // startActivity(initIntents().get(0));
+                // startActivity(initIntents().get(0));
             }
         });
         cbxTerminosCondiciones.setOnClickListener(new View.OnClickListener() {
@@ -99,81 +97,77 @@ public class RegistroUsario extends AppCompatActivity {
                 //startActivity(initIntents().get(1));
             }
         });
-       cbxTerminosCondiciones.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if(cbxTerminosCondiciones.isChecked()){
-                   showDialogTerminosYCondicones();
-               } else {
-                   cbxTerminosCondiciones.isChecked();
-               }
-           }
-       });
-       cbxPoliticasPrivacidad.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if(cbxPoliticasPrivacidad.isChecked()){
-                   showDialogPrivacidad();
-               } else {
-                   cbxPoliticasPrivacidad.isChecked();
-               }
-           }
-       });
+        cbxTerminosCondiciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cbxTerminosCondiciones.isChecked()) {
+                    showDialogTerminosYCondicones();
+                } else {
+                    cbxTerminosCondiciones.isChecked();
+                }
+            }
+        });
+        cbxPoliticasPrivacidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cbxPoliticasPrivacidad.isChecked()) {
+                    showDialogPrivacidad();
+                } else {
+                    cbxPoliticasPrivacidad.isChecked();
+                }
+            }
+        });
 
         btnRegistrarUsuario.setOnClickListener(new View.OnClickListener() {
-                                                   @Override
-                                                   public void onClick(View v) {
-                                                       progressBar.setVisibility(v.VISIBLE);
-                                                       String name, user, confirmUser, password, confirmPassword;
-                                                       name = edtName.getText().toString();
-                                                       user = edtEmail.getText().toString();
-                                                       confirmUser = edtConfirmEmail.getText().toString();
-                                                       password = edtPassword.getText().toString();
-                                                       confirmPassword = edtConfirmPassword.getText().toString();
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(v.VISIBLE);
+                String name, user, confirmUser, password, confirmPassword;
+                name = edtName.getText().toString();
+                user = edtEmail.getText().toString();
+                confirmUser = edtConfirmEmail.getText().toString();
+                password = edtPassword.getText().toString();
+                confirmPassword = edtConfirmPassword.getText().toString();
 
-                                                       if (name.isEmpty() || user.isEmpty() || confirmUser.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
-                                                                showDialogCompleteAll();
-                                                       } else if (!(password.equals(confirmPassword))){
-                                                           showDialogContrasenaNoCoincide();
-                                                       } else if (!(cbxTerminosCondiciones.isChecked())) {
-                                                           showDialogAceptaCondicionesYTerminos();
-                                                       } else if (!(cbxPoliticasPrivacidad.isChecked())) {
-                                                           showDialogAceptaPrivacidad();
-                                                       } else {
-                                                           showDialogoRegistroCompleto();
-                                                       }
-
-                                                   }
-                                               });
-    }
-
-
-/*
-                mAuth.createUserWithEmailAndPassword(user, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                progressBar.setVisibility(v.GONE);
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Toast.makeText(getApplicationContext(), "Cuenta creada con exito!.",
-                                            Toast.LENGTH_SHORT).show();
-                                    clearComponents();
-                                    FirebaseAuth.getInstance().signOut();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(getApplicationContext(), "No se pudo crear la cuenta!.",
-                                            Toast.LENGTH_SHORT).show();
+                if (name.isEmpty() || user.isEmpty() || confirmUser.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    showDialogCompleteAll();
+                    return;
+                } else if (!(password.equals(confirmPassword))) {
+                    showDialogContrasenaNoCoincide();
+                } else if (!(user.equals(confirmPassword))) {
+                    showDialogContrasenaNoCoincide();
+                    return;
+                } else if (!(cbxTerminosCondiciones.isChecked())) {
+                    showDialogAceptaCondicionesYTerminos();
+                    return;
+                } else if (!(cbxPoliticasPrivacidad.isChecked())) {
+                    showDialogAceptaPrivacidad();
+                    return;
+                } else {
+                    showDialogoRegistroCompleto();
+                    mAuth.createUserWithEmailAndPassword(user, password)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressBar.setVisibility(v.GONE);
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Toast.makeText(getApplicationContext(), "Cuenta creada con exito!.",
+                                                Toast.LENGTH_SHORT).show();
+                                        clearComponents();
+                                        FirebaseAuth.getInstance().signOut();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(getApplicationContext(), "No se pudo crear la cuenta!.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
                                 }
-                            }
-                        });
-
+                            });
+                }
 
             }
         });
 
- */
-                /*
         tvYaTieneCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -183,6 +177,7 @@ public class RegistroUsario extends AppCompatActivity {
             }
         });
     }
+
 
     private ArrayList<Intent> initIntents() {
         ArrayList<Intent> listaIntents = new ArrayList<>();
@@ -204,31 +199,33 @@ public class RegistroUsario extends AppCompatActivity {
         edtPassword.setText("");
         edtConfirmPassword.setText("");
     }
-}
-*/
-private void showDialogCompleteAll(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            dialog.cancel();
-        }
-    });
-    builder.setView(R.layout.dialog_completa_todo).create().show();
 
-}
-private void showDialogoRegistroCompleto(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            Intent intent = new Intent(RegistroUsario.this, Login.class);
-            startActivity(intent);
-        }
-    });
-    builder.setView(R.layout.dialog_registro_completo).create().show();
-}
-    private void showDialogContrasenaNoCoincide(){
+
+    private void showDialogCompleteAll() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setView(R.layout.dialog_completa_todo).create().show();
+
+    }
+
+    private void showDialogoRegistroCompleto() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(RegistroUsario.this, Login.class);
+                startActivity(intent);
+            }
+        });
+        builder.setView(R.layout.dialog_registro_completo).create().show();
+    }
+
+    private void showDialogContrasenaNoCoincide() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
@@ -241,62 +238,65 @@ private void showDialogoRegistroCompleto(){
 
     }
 
-    private void showDialogAceptaCondicionesYTerminos(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    private void showDialogAceptaCondicionesYTerminos() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-    builder.setPositiveButton("CERRAR", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            dialog.cancel();
-        }
-    });
-    builder.setView(R.layout.dialog_acepta_terminos_condiciones).create().show();
+        builder.setPositiveButton("CERRAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setView(R.layout.dialog_acepta_terminos_condiciones).create().show();
     }
-    private void showDialogTerminosYCondicones(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-    builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            cbxTerminosCondiciones.isChecked();
-            dialog.cancel();
-        }
-    });
-    builder.setNegativeButton("NO ACEPTO", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            cbxTerminosCondiciones.setChecked(false);
-            dialog.cancel();
-        }
-    });
-    builder.setView(R.layout.dialog_terminos_condiciones).create().show();
+    private void showDialogTerminosYCondicones() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cbxTerminosCondiciones.isChecked();
+                dialog.cancel();
+            }
+        });
+        builder.setNegativeButton("NO ACEPTO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cbxTerminosCondiciones.setChecked(false);
+                dialog.cancel();
+            }
+        });
+        builder.setView(R.layout.dialog_terminos_condiciones).create().show();
     }
-    private void showDialogAceptaPrivacidad(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            dialog.cancel();
-        }
-    });
-    builder.setView(R.layout.dialog_acepta_privacidad).create().show();
+
+    private void showDialogAceptaPrivacidad() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("ACEPTAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.setView(R.layout.dialog_acepta_privacidad).create().show();
     }
-    private void showDialogPrivacidad(){
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setPositiveButton("SI ACEPTO", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            cbxPoliticasPrivacidad.isChecked();
-            dialog.cancel();
-        }
-    });
-    builder.setNegativeButton("NO ACEPTO", new DialogInterface.OnClickListener() {
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            cbxPoliticasPrivacidad.setChecked(false);
-            dialog.cancel();
-        }
-    });
-    builder.setView(R.layout.dialog_privacidad).create().show();
+
+    private void showDialogPrivacidad() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("SI ACEPTO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cbxPoliticasPrivacidad.isChecked();
+                dialog.cancel();
+            }
+        });
+        builder.setNegativeButton("NO ACEPTO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                cbxPoliticasPrivacidad.setChecked(false);
+                dialog.cancel();
+            }
+        });
+        builder.setView(R.layout.dialog_privacidad).create().show();
     }
 }
