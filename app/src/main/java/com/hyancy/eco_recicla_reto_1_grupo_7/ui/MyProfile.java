c4f753eb971e8c733c0b6dc26cfea3cc6c0d3f0a
hyancy <henryyancycaballero@gmail.com>
2023-12-17 21:16:13 -0500

    corrigiendo errores en manifest

---
+++ app/src/main/java/com/hyancy/eco_recicla_reto_1_grupo_7/ui/MyProfile.java
@@ -0,0 +1,15 @@
+package com.hyancy.eco_recicla_reto_1_grupo_7.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hyancy.eco_recicla_reto_1_grupo_7.R;
+
+public class MyProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
